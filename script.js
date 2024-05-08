2774625849b6fb7f759383c6d28b3fd94e78d1ab
Richard van Liessum <richard@liessum.net>
2024-05-08 21:10:58 +0200

    update

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
-
 document.addEventListener('DOMContentLoaded', () => {
-    const jsConfetti = new JSConfetti();
+  const jsConfetti = new JSConfetti()
 
-    function animateButton() {
-        const button = document.getElementById('downloadButton');
-        button.style.animation = 'clickAnimation 0.4s';
-        setTimeout(() => {
-            button.style.animation = '';
-        }, 400);
-    }
+  function animateButton () {
+    const button = document.getElementById('downloadButton')
+    button.style.animation = 'clickAnimation 0.4s'
+    setTimeout(() => {
+      button.style.animation = ''
+    }, 400)
+  }
 
-    window.downloadFile = function() {
-        animateButton();
-        setTimeout(() => {
-            const fileUrl = 'salt_malware.zip'; // Ensure this points to your actual file location
-            fetch(fileUrl, {
-                method: 'HEAD' // Check if the file exists
-            }).then(response => {
-                if (response.ok) {
-                    window.location.href = fileUrl;
-                    // jsConfetti.addConfetti();
-                    jsConfetti.addConfetti({
-                        emojis: ['🦠', '🫥'],
-                        confettiRadius: 6,
-                        confettiNumber: 500,
-                     })
-                } else {
-                    console.error('File not found');
-                    alert('File not found.');
-                }
-            }).catch(console.error);
-        }, 400);
-    }
-});
+  window.downloadFile = function () {
+    animateButton()
+    setTimeout(() => {
+      const fileUrl = 'salt_malware.zip' // Ensure this points to your actual file location
+      fetch(fileUrl, {
+        method: 'HEAD' // Check if the file exists
+      })
+        .then(response => {
+          if (response.ok) {
+            window.location.href = fileUrl
+            // jsConfetti.addConfetti();
+            jsConfetti.addConfetti({
+              emojis: ['🦠', '🫥', '👀'],
+              confettiRadius: 6,
+              confettiNumber: 500
+            })
+          } else {
+            console.error('File not found')
+            alert('File not found.')
+          }
+        })
+        .catch(console.error)
+    }, 400)
+  }
+})
